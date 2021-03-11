f8bf65b53de90c43f58f29c2e8739a68213ad745
Tea Zlatarevic <tea.zlatarevic@gmail.com>
2021-03-11 20:04:27 +0100

    Solution for the example 6

---
+++ ex06/src/index.js
@@ -0,0 +1,13 @@
+function main(numOne, numTwo){
+
+
+    var myQuotient = numOne / numTwo;
+
+    return myQuotient;
+}
+
+console.log(main(5.0,2.0));
+console.log(main(16.5,5.5));
+console.log(main(102.0,25.5));
+
+module.exports = main;
