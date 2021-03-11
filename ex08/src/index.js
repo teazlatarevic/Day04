4c8b861b1d7665f6e8dc2ce1d162efe22bd9f003
Tea Zlatarevic <tea.zlatarevic@gmail.com>
2021-03-12 00:09:12 +0100

    adding the comments

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -1,4 +1,5 @@
 function main() {
+  // Only change code below this line
   var myX = 12;
   var myY = 19;
   var myZ = 24;
@@ -6,6 +7,7 @@ function main() {
   myX += 3;
   myY += 17;
   myZ -= 20;
+  // Only change code above this line
 
   return {
     myX,
