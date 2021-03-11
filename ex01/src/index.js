a1e00d78f931cb08315e93ca27fe323633a373ba
Tea Zlatarevic <tea.zlatarevic@gmail.com>
2021-03-11 20:17:26 +0100

    Using the formatter to make the code more readable on example 1

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,23 +1,17 @@
-function main(numOne, numTwo){
+function main(numOne, numTwo) {
+  var quotient;
+  if (numTwo === 0) {
+    quotient = "Divide by 0! Cannot divide by zero!";
+  } else {
+    quotient = numOne / numTwo;
+  }
 
-    var quotient;
-    if(numTwo === 0){
-        quotient = 'Divide by 0! Cannot divide by zero!'; 
-    } else {
-
-        quotient = numOne / numTwo;
-
-    }
-
-    return quotient;
+  return quotient;
 }
 
-console.log(main(15,3));
-console.log(main(25,5));
-console.log(main(420,10));
-console.log(main(55,0));
-
-
-
+console.log(main(15, 3));
+console.log(main(25, 5));
+console.log(main(420, 10));
+console.log(main(55, 0));
 
 module.exports = main;
