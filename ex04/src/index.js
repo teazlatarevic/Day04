38015b7fbc48b286d6db53e7dacf1e06e3ba67f6
Tea Zlatarevic <tea.zlatarevic@gmail.com>
2021-03-12 00:02:12 +0100

    adding the comments

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,7 +1,11 @@
 function main() {
+    // Only change code below this line
+
 
    var myDecimal = 8.2;
    var myDecimalE = 2.54e-3;
+
+   // Only change code above this line
 
     return {
         myDecimal,
