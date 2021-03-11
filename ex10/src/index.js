d69937a6a7356deaee3c617e56fec4632543178a
Tea Zlatarevic <tea.zlatarevic@gmail.com>
2021-03-11 21:41:00 +0100

    Solution for the example 10

---
+++ ex10/src/index.js
@@ -0,0 +1,14 @@
+function main() {
+  var myFirstName = "Tea";
+  var myLastName = "Zlatarevic";
+  var myHomeTown = "Gradacac";
+
+  return {
+    myFirstName,
+    myLastName,
+    myHomeTown,
+  };
+}
+
+console.log(main());
+module.exports = main;
