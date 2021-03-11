9ece842532eabe071cdade2177d99020fd372d3c
Tea Zlatarevic <tea.zlatarevic@gmail.com>
2021-03-11 15:16:17 +0100

    Solution for the example 4

---
+++ ex04/src/index.js
@@ -0,0 +1,14 @@
+function main() {
+
+   var myDecimal = 8.2;
+   var myDecimalE = 2.54e-3;
+
+    return {
+        myDecimal,
+        myDecimalE
+    };
+}
+
+
+console.log(main());
+module.exports = main;
